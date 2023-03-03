e297130d90dc8e2ab9cd0770a604441db552453d
Tristan Rosales <tristanrosales0@gmail.com>
2023-03-03 22:04:19 +0800

    Adjust team member section

--- application/views/about_page.php
+++ application/views/about_page.php
@@ -112,17 +112,16 @@
     <!-- Team Start -->
     <div class="container mt-5 pt-5 pb-3">
         <div class="d-flex flex-column text-center mb-5">
-            <h4 class="text-secondary mb-3">Team Member</h4>
             <h1 class="display-4 m-0">Meet Our <span class="text-primary">Team Member</span></h1>
         </div>
         <div class="row">
             <div class="col-lg-3 col-md-6">
                 <div class="team card position-relative overflow-hidden border-0 mb-4">
-                    <img class="card-img-top" src="<?php echo base_url(); ?>static/landing_page/img/team-1.jpg" alt="">
+                    <img class="card-img-top" src="<?php echo base_url(); ?>static/landing_page/img/team-1.jpeg">
                     <div class="card-body text-center p-0">
                         <div class="team-text d-flex flex-column justify-content-center bg-light">
-                            <h5>Mollie Ross</h5>
-                            <i>Founder & CEO</i>
+                            <h5>Dr. Rogette S. Petuco</h5>
+                            <i>Owner</i>
                         </div>
                         <div class="team-social d-flex align-items-center justify-content-center bg-dark">
                             <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0" style="width: 36px; height: 36px;" href="#"><i class="fab fa-twitter"></i></a>
@@ -135,11 +134,11 @@
             </div>
             <div class="col-lg-3 col-md-6">
                 <div class="team card position-relative overflow-hidden border-0 mb-4">
-                    <img class="card-img-top" src="<?php echo base_url(); ?>static/landing_page/img/team-2.jpg" alt="">
+                    <img class="card-img-top" src="<?php echo base_url(); ?>static/landing_page/img/team-2.jpeg">
                     <div class="card-body text-center p-0">
                         <div class="team-text d-flex flex-column justify-content-center bg-light">
-                            <h5>Jennifer Page</h5>
-                            <i>Chef Executive</i>
+                            <h5>Carlo Jay Onido</h5>
+                            <i>Veterinary assistant</i>
                         </div>
                         <div class="team-social d-flex align-items-center justify-content-center bg-dark">
                             <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0" style="width: 36px; height: 36px;" href="#"><i class="fab fa-twitter"></i></a>
@@ -152,11 +151,11 @@
             </div>
             <div class="col-lg-3 col-md-6">
                 <div class="team card position-relative overflow-hidden border-0 mb-4">
-                    <img class="card-img-top" src="<?php echo base_url(); ?>static/landing_page/img/team-3.jpg" alt="">
+                    <img class="card-img-top" src="<?php echo base_url(); ?>static/landing_page/img/team-3.jpeg">
                     <div class="card-body text-center p-0">
                         <div class="team-text d-flex flex-column justify-content-center bg-light">
-                            <h5>Kate Glover</h5>
-                            <i>Doctor</i>
+                            <h5>Christian Sumande</h5>
+                            <i>Veterinary assistant</i>
                         </div>
                         <div class="team-social d-flex align-items-center justify-content-center bg-dark">
                             <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0" style="width: 36px; height: 36px;" href="#"><i class="fab fa-twitter"></i></a>
@@ -169,11 +168,11 @@
             </div>
             <div class="col-lg-3 col-md-6">
                 <div class="team card position-relative overflow-hidden border-0 mb-4">
-                    <img class="card-img-top" src="<?php echo base_url(); ?>static/landing_page/img/team-4.jpg" alt="">
+                    <img class="card-img-top" src="<?php echo base_url(); ?>static/landing_page/img/team-4.jpeg">
                     <div class="card-body text-center p-0">
                         <div class="team-text d-flex flex-column justify-content-center bg-light">
-                            <h5>Lilly Fry</h5>
-                            <i>Trainer</i>
+                            <h5>Jestoni Beloya</h5>
+                            <i>Groomer</i>
                         </div>
                         <div class="team-social d-flex align-items-center justify-content-center bg-dark">
                             <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0" style="width: 36px; height: 36px;" href="#"><i class="fab fa-twitter"></i></a>
